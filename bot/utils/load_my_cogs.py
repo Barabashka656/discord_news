e08b88c68a906afd34dd844e9734b5f19dc24622
barabashka656 <alekseithebestofthebest@gmail.com>
2023-06-22 13:13:13 +0300

    fix

--- bot/utils/load_my_cogs.py
+++ bot/utils/load_my_cogs.py
@@ -1,10 +1,12 @@
 import os
+import logging
 
 from bot.data.config import COGS_FOLDER
 from bot.data.loader import bot
 
 
 def load_cogs(ROOT_DIR):
+    logging.exception(os.listdir(os.path.join(ROOT_DIR, 'bot')))
     dir = os.path.join(ROOT_DIR, COGS_FOLDER)
     for name in os.listdir(dir):
         if name.endswith(".py") and os.path.isfile(f"{COGS_FOLDER}/{name}"):
