b49106e9c75788693d0c6149fea4002632775e3c
barabashka656 <alekseithebestofthebest@gmail.com>
2023-06-22 14:12:28 +0300

    fix

--- bot/utils/load_my_cogs.py
+++ bot/utils/load_my_cogs.py
@@ -9,11 +9,12 @@ def load_cogs(ROOT_DIR):
     logging.exception(os.listdir(os.path.join(ROOT_DIR, 'bot', 'cogs')))
     dir = os.path.join(ROOT_DIR, COGS_FOLDER)
     print(dir)
+    print(os.listdir(os.path.join(ROOT_DIR, 'bot', 'cogs')))
     for name in os.listdir(os.path.join(ROOT_DIR, 'bot', 'cogs')):
         print(name, 'name1')
         print(os.path.join(COGS_FOLDER, name), 'name2')
         print(os.path.isfile(os.path.join(ROOT_DIR, 'bot', 'cogs', name)), 'name3')
-        if name.endswith(".py") and os.path.isfile(os.path.join(COGS_FOLDER, name)):
+        if name.endswith(".py") and os.path.isfile(os.path.join('bot', 'cogs', name)):
             print(name)
             print(f"bot.cogs.{name[:-3]}")
             bot.load_extension(f"bot.cogs.{name[:-3]}")
