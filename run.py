6e5f12d9da2c7a8cd083e1ac86e190168bdb1001
barabashka656 <alekseithebestofthebest@gmail.com>
2023-06-22 12:13:11 +0300

    fix

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
-from bot.data.config import DISCORD_TOKEN
-from bot.data.loader import bot
-from bot.utils.load_my_cogs import load_cogs
-from bot.utils.my_logger import configure_logger
+import os
+import logging
+
+#from bot.data.config import DISCORD_TOKEN
+#from bot.data.loader import bot
+#from bot.utils.load_my_cogs import load_cogs
+#from bot.utils.my_logger import configure_logger
 
 
 def main():
-    configure_logger()
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    
+    #configure_logger()
+    logging.exception(ROOT_DIR)
+    return 
     load_cogs()
     bot.run(DISCORD_TOKEN)
 
