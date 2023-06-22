37a3318213a6d878e0757d748719f50161651cd2
barabashka656 <alekseithebestofthebest@gmail.com>
2023-06-22 14:08:34 +0300

    fix

--- bot/utils/load_my_cogs.py
+++ bot/utils/load_my_cogs.py
@@ -12,7 +12,7 @@ def load_cogs(ROOT_DIR):
     for name in os.listdir(os.path.join(ROOT_DIR, 'bot', 'cogs')):
         print(name, 'name1')
         print(os.path.join(COGS_FOLDER, name), 'name2')
-        print(os.path.isfile(os.path.join(COGS_FOLDER, name)), 'name3')
+        print(os.path.isfile(os.path.join(ROOT_DIR, 'bot', 'cogs', name)), 'name3')
         if name.endswith(".py") and os.path.isfile(os.path.join(COGS_FOLDER, name)):
             print(name)
             print(f"bot.cogs.{name[:-3]}")
