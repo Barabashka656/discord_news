54ba7f98386efbd5f9413f69e5b3fb3528181ab8
barabashka656 <alekseithebestofthebest@gmail.com>
2023-06-22 13:31:42 +0300

    fix

--- bot/utils/load_my_cogs.py
+++ bot/utils/load_my_cogs.py
@@ -9,6 +9,6 @@ def load_cogs(ROOT_DIR):
     logging.exception(os.listdir(os.path.join(ROOT_DIR, 'bot', 'cogs')))
     dir = os.path.join(ROOT_DIR, COGS_FOLDER)
     print(dir)
-    for name in os.listdir(ROOT_DIR, 'bot', 'cogs'):
+    for name in os.listdir(os.path.join(ROOT_DIR, 'bot', 'cogs')):
         if name.endswith(".py") and os.path.isfile(os.path.join(COGS_FOLDER, name)):
             bot.load_extension(f"bot.cogs.{name[:-3]}")
